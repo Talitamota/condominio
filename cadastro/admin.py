005141b23e0c05e184834fbd51731424e3c0f4c0
Talitmota <talitamotan@gmail.com>
2018-06-24 11:20:03 -0300

    adicionando campo numero a listagem da pag de administração

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Bloco
 
-admin.site.register(Bloco)
-# Register your models here.
+
+class BlocoAdmin(admin.ModelAdmin):	
+	list_display = ('nome','numero')
+
+
+admin.site.register(Bloco, BlocoAdmin)
