9caffda606802cf093c8f69956ef541aa8afe403
Talitmota <talitamotan@gmail.com>
2018-06-24 22:33:26 -0300

    implementando model morador

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+
 # Create your models here.
 class Bloco(models.Model):
 
@@ -19,19 +20,61 @@ class Bloco(models.Model):
 
 
 class Morador(models.Model):
-	pass
+
+	numero_apartamento = models.ForeignKey('Apartamento',
+			verbose_name=u'Número do Apartamento',on_delete=models.CASCADE)
+
+	nome = models.CharField(max_length=50)
+
+	sobrenome = models.CharField(max_length=50)
+
+	email = models.CharField(max_length=50,
+			null=True, blank=True)
+
+	telefone1 = models.CharField('Telefone residencial',	
+			max_length=30, null=True, blank=True)
+
+	telefone2 = models.CharField('Telefone celular',
+			max_length=30, null=True, blank=True)
+	
+	numero_cpf = models.CharField('CPF',
+			max_length=50, null=True, blank=True)
+	
+	rg = models.CharField('RG',
+			max_length=50, null=True, blank=True)
+
+	orgao_expeditor = models.CharField(u'Orgão expeditor',
+			max_length=50, null=True, blank=True)
+	
+	data_nascimento = models.DateField('Data de nascimento',
+			null=True, blank=True)
+	
+	sexo = models.CharField(max_length=50,
+			null=True, blank=True, default='', choices=(
+				('Masculino', 'Masculino'),
+				('Feminino', 'Feminino')))
+
+	def nome_completo(self):
+		return '%s %s' %(self.nome, self.sobrenome)
+
+
+	def __str__(self):
+		return self.nome_completo()
 
 	class Meta:
 		verbose_name = 'Morador'
 		verbose_name_plural = 'Moradores'
 
+
 class Apartamento(models.Model):
 
 	numero = models.CharField(max_length=4)
 
-	bloco = models.ForeignKey(Bloco, on_delete=models.CASCADE)
+	bloco = models.ForeignKey(Bloco,
+			on_delete=models.CASCADE)
 
-	moradores = models.ManyToManyField(Morador)
+	def __str__(self):
+		return self.numero
 
 	class Meta:
 		verbose_name = 'Apartamento'
